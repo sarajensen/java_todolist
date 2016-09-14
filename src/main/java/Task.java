f366c377f25d6c2f38b9764dab9729e8c3fefc86
Sara Jensen <sara@>
2016-09-14 16:46:51 -0700

    created tests and logic to store all instances of the Task class, store the date the user created their task, and set the completed method to false by default.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,34 @@
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Task {
   private String mDescriptionOfTask;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
+  private static List<Task> instances = new ArrayList<Task>();
 
   public Task(String descriptionOfTask) {
     mDescriptionOfTask = descriptionOfTask;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
+    instances.add(this);
   }
 
   public String getDescriptionOfTask() {
     return mDescriptionOfTask;
   }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
+  public static List<Task> all() {
+    return instances;
+  }
+
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,10 +1,32 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
 
 public class TaskTest {
   @Test
   public void Task_instantiatesCorrectly_String() {
     Task testTask = new Task("Mow the Lawn");
     assertEquals("Mow the Lawn", testTask.getDescriptionOfTask());
+  }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Task testTask = new Task("Mow the lawn");
+    assertEquals(false, testTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task testTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), testTask.getCreatedAt().getDayOfWeek());
+  }
+
+  @Test
+  public void all_retrievesAllInstantiatedObjects_true() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Return that book");
+    assertEquals(true, Task.all().contains(firstTask));
+    assertEquals(true, Task.all().contains(firstTask));
   }
 }
